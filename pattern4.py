ddcf500706f1b87501d3c8bf4d170f3e0ac5eb4a
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2021-08-03 20:58:25 +0530

    Add files via upload

---
+++ pattern4.py
@@ -0,0 +1,16 @@
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# 666666
+n = int(input("Enter the value of n "))
+i = 1
+while i <= n:
+	j=1
+	p=i 
+	while j <= i:
+		print(p,end="")
+		j = j + 1
+	print()
+	i = i + 1
---
+++ pattern5.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the value of n "))
+i = 1
+while i <= n:
+	j=1
+	p=i 
+	while j <= i:
+		print(p,end="")
+		j = j + 1
+	print()
+	i = i + 1
+	
